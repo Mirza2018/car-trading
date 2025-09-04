325d66d38647672979cd412ecb6091d9f8dae44f
mirza2018 <mirza.eee.4th@gmail.com>
2025-09-04 16:13:33 +0600

    add

--- src/helpers/config/envConfig.js
+++ src/helpers/config/envConfig.js
@@ -1,11 +1,24 @@
 export const getBaseUrl = () => {
-  return process.env.NEXT_PUBLIC_BASE_URL || "http://10.10.10.30:8000/api/v1";
+  return process.env.NEXT_PUBLIC_BASE_URL;
 };
 
 export const getImageUrl = () => {
-  return process.env.NEXT_PUBLIC_IMAGE_URL || "http://10.10.10.30:8000/";
+  return process.env.NEXT_PUBLIC_IMAGE_URL ;
 };
 
 export const getSocketUrl = () => {
-  return process.env.NEXT_PUBLIC_SOCKET_URL || "http://10.10.10.30:8001";
+  return process.env.NEXT_PUBLIC_SOCKET_URL ;
 };
+
+
+// export const getBaseUrl = () => {
+//   return process.env.NEXT_PUBLIC_BASE_URL || "http://10.10.10.30:8000/api/v1";
+// };
+
+// export const getImageUrl = () => {
+//   return process.env.NEXT_PUBLIC_IMAGE_URL || "http://10.10.10.30:8000/";
+// };
+
+// export const getSocketUrl = () => {
+//   return process.env.NEXT_PUBLIC_SOCKET_URL || "http://10.10.10.30:8001";
+// };
